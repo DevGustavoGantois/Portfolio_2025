67e2104c6b9a90bd43d40eb8f6933592db452482
Gustavo Gantois <gugagantois@gmail.com>
2025-09-13 16:09:16 -0300

    feat:Adding project for test.

--- src/app/s-projects.tsx
+++ src/app/s-projects.tsx
@@ -8,7 +8,9 @@ export function HomeProjects() {
       {homeProjectData.map((item, index) => (
         <div key={index} className="w-full flex-1 mb-16">
           <article className="flex items-center justify-center mb-8">
-            <h1 className="text-white text-center text-4xl lg:text-[40px]">{item.title}</h1>
+            <h1 className="text-white text-center text-4xl lg:text-[40px]">
+              {item.title}
+            </h1>
           </article>
 
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 w-full">
@@ -18,24 +20,34 @@ export function HomeProjects() {
                 className="w-full flex-1 border border-white/10 rounded-2xl py-5 px-2"
               >
                 <div className="flex flex-col justify-center lg:justify-start items-center gap-6 px-2 py-8">
-                  <figure className="w-full bg-black/5">
+                  <figure className="w-full bg-black/5 overflow-hidden rounded-lg">
                     {project.img && (
-                      <Link href={project.href || "#"} target={project.href ? "_blank" : undefined}>
-                      <Image
-                        src={project.img}
-                        width={1200}
-                        height={1200}
-                        alt={project.title}
-                        className="w-full h-full rounded-lg transition-all duration-1000 hover:scale-[1.2]"
-                      />
+                      <Link
+                        href={project.href || "#"}
+                        target={project.href ? "_blank" : undefined}
+                      >
+                        <div className="relative w-full h-0 pb-[100%]">
+                          {" "}
+                          <Image
+                            priority
+                            src={project.img}
+                            alt={project.title}
+                            fill
+                            className="object-cover rounded-lg transition-transform duration-1000 hover:scale-105"
+                          />
+                        </div>
                       </Link>
                     )}
                   </figure>
 
                   <article className="flex flex-col gap-4 justify-start w-full">
                     <div className="flex items-center justify-between">
-                      <h1 className="text-white/90 text-base lg:text-xl">{project.title}</h1>
-                      <h2 className="text-white/70 text-base lg:text-xl">{project.date}</h2>
+                      <h1 className="text-white/90 text-base lg:text-xl">
+                        {project.title}
+                      </h1>
+                      <h2 className="text-white/70 text-base lg:text-xl">
+                        {project.date}
+                      </h2>
                     </div>
                     <p className="text-white/70 text-base lg:text-xl text-start max-w-[700px]">
                       {project.description}
@@ -54,7 +66,10 @@ export function HomeProjects() {
                       </div>
 
                       <div className="flex items-center justify-end gap-2">
-                        <project.iconDesign className="text-white/50" size={24} />
+                        <project.iconDesign
+                          className="text-white/50"
+                          size={24}
+                        />
                         <project.iconDev className="text-white/50" size={24} />
                       </div>
                     </div>
