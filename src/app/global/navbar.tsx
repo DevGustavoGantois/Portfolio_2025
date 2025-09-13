ff6ad8a3a8243ebb873ee282fb50bc3a31a67c28
Gustavo Gantois <gugagantois@gmail.com>
2025-09-13 11:40:35 -0300

    feat:Create v-data for information landing-page.

---
+++ src/app/global/navbar.tsx
@@ -0,0 +1,9 @@
+export function Navbar() {
+    return (
+        <>
+        <header className="max-w-[1440px] mx-auto p-8">
+            
+        </header>
+        </>
+    )
+}
---
+++ src/data/v-data.ts
@@ -0,0 +1,34 @@
+export const navbarData = [
+    {
+        Logo: "/Logo.svg",
+        href: "/"
+    },
+    {
+        name: "Home",
+        href: "#home"
+    },
+    {
+        name: "Sobre mim",
+        href: "#about"
+    },
+    {
+        name: "Projetos",
+        href: "#project"
+    },
+    {
+        name: "Depoimentos",
+        href: "#testimonials"
+    },
+    {
+        name: "FAQ",
+        href: "#faq"
+    },
+    {
+        name: "Contato",
+        href: "#contact",
+    },
+    {
+        btnText: "Entrar em contato via Whatsapp",
+        href: "#"
+    }
+]
