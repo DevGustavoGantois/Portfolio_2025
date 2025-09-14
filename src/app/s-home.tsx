7f933f2cbe5a69b5aa7e597fe55404911c9bc2ed
Gustavo Gantois <gugagantois@gmail.com>
2025-09-13 22:18:21 -0300

    docs:Fix docs.

--- src/app/s-home.tsx
+++ src/app/s-home.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { cn } from "@/lib/utils";
-import { Globe,  } from "lucide-react";
+import { Globe} from "lucide-react";
 import Link from "next/link";
 
 export function HomeHero() {
