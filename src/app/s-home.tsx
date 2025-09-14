f53c8691dc29573c5b55527e4334da95155f17f2
Gustavo Gantois <gugagantois@gmail.com>
2025-09-13 22:14:22 -0300

    remove:Remove spothlight.

--- src/app/s-home.tsx
+++ src/app/s-home.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { cn } from "@/lib/utils";
-import { Globe, Spotlight } from "lucide-react";
+import { Globe,  } from "lucide-react";
 import Link from "next/link";
 
 export function HomeHero() {
@@ -11,11 +11,6 @@ export function HomeHero() {
           "pointer-events-none absolute inset-0 [background-size:40px_40px] select-none",
           "[background-image:linear-gradient(to_right,#171717_1px,transparent_1px),linear-gradient(to_bottom,#171717_1px,transparent_1px)]",
         )}
-      />
-
-      <Spotlight
-        className="-top-40 left-0 md:-top-20 md:left-60"
-        fill="white"
       />
       <div className="relative z-10 mx-auto w-full max-w-8xl p-4 pt-20 md:pt-0">
         <h1 className="bg-opacity-50 bg-gradient-to-b from-neutral-50 to-[#FF6600] bg-clip-text text-center text-4xl font-bold text-transparent md:text-7xl">
